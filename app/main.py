02e5078c7cca0c8e8a933343474f4f2dba349686
Sergey <sergey@gmail.com>
2026-03-18 23:42:52 +0300

    add webhook endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"status": "ok"}
+
+@app.post("/webhook")
+async def telegram_webhook(request: Request):
+    data = await request.json()
+    print(data)
+    return {"ok": True}
