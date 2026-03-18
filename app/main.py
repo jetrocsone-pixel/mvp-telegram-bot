d4a9dc0f2cec4589b5bb8dec9c3ab1fbd9e7c87e
Sergey <sergey@gmail.com>
2026-03-18 23:47:59 +0300

    add bot response

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Request
+import requests
 
 app = FastAPI()
+
+BOT_TOKEN = "8591079372:AAGncRoWu_xNfHzTMG879XwB7LEtvxvIyZY"
 
 @app.get("/")
 def home():
@@ -9,5 +12,18 @@ def home():
 @app.post("/webhook")
 async def telegram_webhook(request: Request):
     data = await request.json()
-    print(data)
+
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+
+        reply_text = f"Ты написал: {text}"
+
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+        requests.post(url, json={
+            "chat_id": chat_id,
+            "text": reply_text
+        })
+
     return {"ok": True}
